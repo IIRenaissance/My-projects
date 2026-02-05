955cfc7c4b4fb5d567e8e336bc801cc2f675d94d
IIRenaissance <helenisticy@gmail.com>
2026-02-05 12:43:12 +0300

    Dosya kaydetme özelliği eklendi

--- harcama-takibi.py
+++ harcama-takibi.py
@@ -1,10 +1,29 @@
 print("===Harcama Takip===")
-toplam = 0
-print(f"Toplam harcama: {toplam} Tl")
+
+#eski veriler
+try:
+    dosya=open("harcamalar.txt", "r")
+    satirlar = dosya.readlines()
+    dosya.close()
+    
+    toplam = 0
+    for satir in satirlar:
+        toplam = toplam + int(satir)
+    
+    print(f"Önceki toplam harcama: {toplam} TL")
+
+except:
+    toplam = 0
+    print(f"Toplam harcama: {toplam} Tl")
 
 harcama=int(input("Harcama miktarı:"))
 toplam= toplam + harcama
 print(f"Toplam harcama : {toplam} TL")
+
+#dosyaya kaydetme
+dosya= open("harcamalar.txt","a")
+dosya.write(f"{harcama} \n")
+dosya.close()
 
 devam = input("Başka harcama var mı? (e/h):")
 
@@ -12,6 +31,11 @@ while devam == "e":
     harcama=int(input("Harcama miktarı:"))
     toplam= toplam + harcama
     print(f"Toplam harcama : {toplam} TL")
+
+    #dosya ekleme
+    dosya = open("harcamalar.txt", "a")
+    dosya.write(f"{harcama}\n")
+    dosya.close()
 
     devam = input("Başka harcama var mı? (e/h):")
 
